79f9e40ad7132e661a4ceeee884afd4a85d598a5
YashM1234 <yashmungolerpg@gmail.com>
2025-05-01 14:37:25 +0530

    Leetcode Q-44 WAP for Wildcard Matching...

---
+++ src/main/java/com/bootcoding/dsa/leetcodel1/WildcardMatching.java
@@ -0,0 +1,47 @@
+package com.bootcoding.dsa.leetcodel1;
+
+/*
+Leetcode Q-44 WAP for Wildcard Matching
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+ */
+public class WildcardMatching {
+    public static void main(String[] args) {
+        String s = "aa";
+        String p = "a";
+        //Method Calling
+        boolean result = isMatch(s, p);
+        System.out.println(result);
+    }
+
+    //Method Definition
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        for(int j = 1; j <= n; j++){
+            if(p.charAt(j - 1) == '*'){
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for(int i = 1; i <= m;i++){
+            for(int j = 1; j <= n; j++){
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if(pc == sc || pc == '?'){
+                    dp[i][j] = dp[i - 1][j -  1];
+                }else if(pc == '*'){
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
