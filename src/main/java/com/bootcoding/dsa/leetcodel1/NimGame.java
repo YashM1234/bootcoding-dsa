f3221dd390c4d4119f735ccd1bdd5842a3febe80
YashM1234 <yashmungolerpg@gmail.com>
2025-05-10 15:48:54 +0530

    Leetcode Q-292 WAP for Nim Game...

---
+++ src/main/java/com/bootcoding/dsa/leetcodel1/NimGame.java
@@ -0,0 +1,26 @@
+package com.bootcoding.dsa.leetcodel1;
+
+/*
+Leetcode Q-292 WAP for Nim Game
+You are playing the following Nim Game with your friend:
+Initially, there is a heap of stones on the table.
+You and your friend will alternate taking turns, and you go first.
+On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+The one who removes the last stone is the winner.
+Given n, the number of stones in the heap, return true if you can win the game assuming
+both you and your friend play optimally, otherwise return false.
+ */
+public class NimGame {
+    public static void main(String[] args) {
+        int n = 4;
+        //Method Calling
+        boolean result = canWinNim(n);
+        System.out.println(result);
+
+    }
+
+    //Method Definition
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
